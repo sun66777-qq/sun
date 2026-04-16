ec78ca9cd04e5211d6bf5b9f4860ef04f7209c1e
liqing <3497644821@qq.com>
2026-04-16 08:02:29 +0000

    将整个页面的背景改为浅蓝色

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -36,7 +36,7 @@ const courses = [
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-blue-50">
       {/* 导航栏 */}
       <nav className="bg-white shadow-md">
         <div className="container mx-auto px-4 py-3 flex justify-between items-center">
